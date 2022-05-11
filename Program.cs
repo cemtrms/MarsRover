3037da253b7875c49e05d97c0dd15031d4bbcc02
cemtrms <cemturmus@outlook.com>
2022-05-11 23:02:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using MarsRover;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] tokens = Console.ReadLine().Split();
+        int maxX = int.Parse(tokens[0]);
+        int maxY = int.Parse(tokens[1]);
+        do
+        {
+            var positionLine = Console.ReadLine();
+            if (string.IsNullOrEmpty(positionLine))
+                break;
+            Rover rover = ParseRover(positionLine);
+            var moveLine = Console.ReadLine();
+
+            foreach (char move in moveLine)
+            {
+                switch (move)
+                {
+                    case 'L':
+                        rover.TurnLeft();
+                        break;
+                    case 'R':
+                        rover.TurnRight();
+                        break;
+                    case 'M':
+                        rover.Forward();
+                        rover.Limit(maxX, maxY);
+                        break;
+                }
+            }
+            Console.WriteLine(rover);
+        } while (true);
+    }
+    private static Rover ParseRover(string positionLine)
+    {
+        var positionTokens = positionLine.Split();
+        int[] pos = new int[]
+        {
+            int.Parse(positionTokens[0]),
+            int.Parse(positionTokens[1]),
+        };
+        char heading = positionTokens[2][0];
+        return new Rover(pos, heading);
+    }
+}
---
+++ Rover.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    class Rover
+    {
+        public int[] Pos { get; private set; }
+        public char Heading { get; private set; }
+        public Rover(int[] pos, char heading)
+        {
+            Pos = pos;
+            Heading = heading;
+        }
+        /// <summary>
+        /// Belirlenen sınırlar dışına çıkmayıp ilk pozisyonuna alır
+        /// </summary>
+        /// <param name="maxX"></param>
+        /// <param name="maxY"></param>
+        public void Limit(int maxX, int maxY)
+        {
+            if (Pos[0] > maxX)
+            {
+                Pos[0] = maxX;
+            }
+            if (Pos[1] > maxY)
+            {
+                Pos[1] = maxY;
+            }
+            if (Pos[0] < 0)
+            {
+                Pos[0] = 0;
+            }
+            if (Pos[1] < 0)
+            {
+                Pos[1] = 1;
+            }
+        }
+        /// <summary>
+        /// Sola dönüşlerde yönü günceller
+        /// </summary>
+        public void TurnLeft()
+        {
+            switch (Heading)
+            {
+                case 'N': Heading = 'W'; break;
+                case 'W': Heading = 'S'; break;
+                case 'S': Heading = 'E'; break;
+                case 'E': Heading = 'N'; break;
+                default: throw new ArgumentException("unknown heading");
+            }
+        }
+        /// <summary>
+        /// Sağa dönüşlerde yönü güncelleer
+        /// </summary>
+        public void TurnRight()
+        {
+            switch (Heading)
+            {
+                case 'N': Heading = 'E'; break;
+                case 'E': Heading = 'S'; break;
+                case 'S': Heading = 'W'; break;
+                case 'W': Heading = 'N'; break;
+                default: throw new ArgumentException("unknown heading");
+            }
+        }
+        /// <summary>
+        /// Yöne göre pozisyonu ileletir
+        /// </summary>
+        public void Forward()
+        {
+            switch (Heading)
+            {
+                case 'N': Pos[1]++; break;
+                case 'E': Pos[0]++; break;
+                case 'S': Pos[1]--; break;
+                case 'W': Pos[0]--; break;
+                default: throw new ArgumentException("unknown heading");
+            }
+        }
+
+        public override string ToString()
+        {
+            return Pos[0] + " " + Pos[1] + " " + Heading;
+        }
+    }
+}
